f554f317a3c502859a7f5f9b4a0f00176b72bda4
Andrew Cabrera <andrewcabrera7@gmail.com>
2019-09-23 16:40:18 -0700

    added a routes file and handlebars files

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = process.env.port || 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+const routes = require('./routes/routes');
+
+app.use(routes);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
