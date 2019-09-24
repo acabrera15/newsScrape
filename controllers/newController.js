fd32d00159bd1e284c6a883ce26b1ef4b73cd5a8
Andrew Cabrera <andrewcabrera7@gmail.com>
2019-09-24 16:02:08 -0700

    content is not added to the display from TIME

--- controllers/newController.js
+++ controllers/newController.js
@@ -8,23 +8,46 @@ Router.get("/", function(req, result) {
   axios
     .get("https://time.com/section/tech/")
     .then(function(res) {
-        const $ = cheerio.load(res.data);
+      const $ = cheerio.load(res.data);
+      var newsObjs = [];
 
-        $('.partial.media').each(function(i, element) {
-            var headline = $(element).find('.headline').text();
-            var summary  = $(element).find('.summary.margin-8-bottom.desktop-only').text();
-            var URL  = $(element).find('a').attr('href');
-            console.log("HEAD" +headline)
-            console.log("SUM" + summary)
-            console.log("URL" + URL)
+      //gets newInfo from website
+      $(".partial.media").each(function(i, element) {
+        var newsObj = {
+          headline: "",
+          summary: "",
+          URL: "",
+          image: ""
+        };
+        newsObj.headline = $(element)
+          .find(".headline")
+          .text()
+          .replace(/(\r\n|\n|\r)/gm, "")
+          .trim();
+        newsObj.summary = $(element)
+          .find(".summary.margin-8-bottom.desktop-only")
+          .text()
+          .replace(/(\r\n|\n|\r)/gm, "")
+          .trim();
+        newsObj.URL = $(element)
+          .find("a")
+          .attr("href")
+          .replace(/(\r\n|\n|\r)/gm, "")
+          .trim();
 
-        });
+        console.log(
+          $(element)
+            .find('.inner-container').text()
+        );
+        console.log(newsObj);
 
+        newsObjs.push(newsObj);
+      });
+      result.render("index", { news: newsObjs });
     })
     .catch(function(err) {
       if (err) throw err;
     });
-  var $ = cheerio.load(res.data);
 });
 
 module.exports = Router;
