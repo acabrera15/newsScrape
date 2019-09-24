5e3376e40a489d21e5fa046a5177fa3ba716098d
Andrew Cabrera <andrewcabrera7@gmail.com>
2019-09-24 12:02:12 -0700

    added a model and handlebars

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
-const express = require("express");
-const app = express();
-const port = process.env.port || 3000;
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
 
-const routes = require('./routes/routes');
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+app.use(express.static(path.join(__dirname, "/public")));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+const routes = require("./routes/routes");
 
 app.use(routes);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
