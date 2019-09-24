e9fe6910884233ac1e06362172fd6fa863dc9886
Andrew Cabrera <andrewcabrera7@gmail.com>
2019-09-24 12:04:45 -0700

    added data types to model

--- models/newsModel.js
+++ models/newsModel.js
@@ -2,5 +2,9 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
 
 const News = mongoose.model('News', {
-    
-})
+    headline: String,
+    summary: String,
+    URL: String
+});
+
+module.exports = News;
